ce937a9063fa7ef0d108b033a058a41f4946631f
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-04-24 16:19:53 +0100

    Initial commit Files read included already.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+headers = ["data", "visitors"]
+data = pd.read_csv("visitors.csv", skiprows=4, names=headers)
+# print(data.head())
+
+headers = ["data", "visitors_new"]
+data_new = pd.read_csv("visitors-new.csv", skiprows=4, names=headers)
+print(data_new.head())
