5c88804523dba8ff501eeda9aaaa8c7122a7e4f1
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-07-05 15:51:03 +0200

    Finished without x axis values labels Ref: https://www.pythonforengineers.com/analyse-visitors-to-a-website/

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-headers = ["data", "visitors"]
+headers = ["date", "visitors"]
 data = pd.read_csv("visitors.csv", skiprows=4, names=headers)
-# print(data.head())
+data.head()
 
-headers = ["data", "visitors_new"]
+headers = ["date", "visitors_new"]
 data_new = pd.read_csv("visitors-new.csv", skiprows=4, names=headers)
-# print(data_new.head())
+data_new.head()
 
 data_combined = pd.merge(data, data_new)
-data_combined.sort_values(by=["data"], inplace=True)
-data_combined.set_index("data", inplace=True)
-# print(data_combined.head())
 data_combined.head()
-
+data_combined.sort_values(by=["date"], inplace=True)
+data_combined.set_index("date", inplace=True)
+data_combined.head()
 data_combined.plot()
 plt.show()
